c3771f87c6fcf780a461809b3c640e9d06de61b8
farwanoreen <farwanoreen009@gmail.com>
2023-09-08 14:43:53 -0700

    second methid

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
 import { Hero } from '@/components/home/Hero'
 import Image from 'next/image'
+import { BiRightArrowAlt } from 'react-icons/bi'
 
 export default function Home() {
   return (
     <main className="">
-            <Hero />
+      <Hero />
+
+      <section className='text-center section py-16'>
+        <div>
+          <h1 className='text-7xl zinc-text w-6/12 font-medium mx-auto '>
+            We are obsessed with Customer Flow<span className='text-7xl text-yellow-400 font-extrabold'> .</span>
+          </h1>
+          <p className='text-xl   w-7/12  mx-auto mt-10'>The continuous movement of your customers along the path to purchase is vital to your business success. Our purpose is to help you build and accelerate Customer Flow.</p>
+          <div className='flex justify-center mt-8'>
+            <button className='btn mt-4 yellow-bg text-white text-lg flex justify-center items-center gap-2' >Find out more <BiRightArrowAlt /></button>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Hero = () => {
     return (
-        <section className='zinc-bg py-52 my-10 '>
+        <section className='zinc-bg py-40  '>
             <div className='container text-center '>
                 <h1 className=' home-hero-title'>TechSavy Group</h1>
-                <p className='text-3xl font-normal pt-4'>Empowering Brands Digitally!</p>
+                <p className='text-3xl font-normal pt-4'>Empowering Brands Digitally! <span className='text-4xl text-yellow-400 font-extrabold'> .</span></p>
             </div>
         </section>
     )
