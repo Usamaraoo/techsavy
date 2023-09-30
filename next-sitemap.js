375afceeb9f404f877dd6b495002b29b774a5482
UsamaKaleemTechmate <usamakaleem@techmatetech.com>
2023-09-30 23:02:16 +0500

    sitemap change location

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://techsavygroup.com/',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateRobotsTxt: true,
+    // Additional configuration options can be added here
+  };
