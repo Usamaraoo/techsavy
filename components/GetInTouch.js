0204928323cd5fb0e8dcc2ded6a0d33a3334129c
Usamaraoo <usamakaleem505@gmail.com>
2023-10-06 00:07:13 +0500

    format string

--- components/GetInTouch.js
+++ components/GetInTouch.js
@@ -10,12 +10,12 @@ const GetInTouch = () => {
         <div className="container mx-auto">
           <h1 className="text-4xl font-[600] dot-title">Get in touch today</h1>
           <p className=" w-11/12  md:w-7/12  mx-auto mt-10">
-            Whether you have questions, ideas, or simply want to connect, we
+            {`Whether you have questions, ideas, or simply want to connect, we
             welcome the opportunity to hear from you. Our team is always here to
             assist you in any way we can. Feel free to reach out to us via
             phone, email, or social media, and we'll respond promptly. Your
             feedback and inquiries are important to us, and we look forward to
-            engaging with you soon.
+            engaging with you soon.`}
           </p>
           <div className="flex justify-center mt-8">
             <Link
